82b0737fd0fc42120f70ec25c3db90e6d01c11c1
RMB MYR <58590065+RMB-MYR@users.noreply.github.com>
2025-01-27 07:35:16 +0100

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,16 @@
+public class Tree {
+  public static void main (String[] args){
+    System.out.println ("Tree");
+    System.out.println ("Hi, I am Rembo and I am learning to code Java");
+    System.out.println("Today, I am going to plant a tree:");
+    System.out.println("        "); 
+    System.out.println("      *   "); 
+    System.out.println("     ***  ");
+    System.out.println("    *****  ");
+    System.out.println("   ******* ");
+    System.out.println("  ********* ");
+    System.out.println(" *********** ");
+    System.out.println("     ***  ");
+    System.out.println("     ***  ");   
+  }
+}
