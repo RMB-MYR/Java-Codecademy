9ba5ab3dea06226a7979bd22ab47783161ad7e5d
RMB MYR <58590065+RMB-MYR@users.noreply.github.com>
2025-01-27 08:07:22 +0100

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,15 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Rembo";
+    int age = 28;
+    double desiredSalary = 103500;
+    char gender = 'm';
+    boolean lookingForJob = true;
+
+    System.out.println("Your next employee");
+    System.out.println(age);
+    System.out.println("Is he looking for a Job?:");
+    System.out.println(lookingForJob);
+
+	}
+}
