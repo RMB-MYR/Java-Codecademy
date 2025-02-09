c4cacc485990f3ba5167551e0034be94f798ef09
RMB MYR <58590065+RMB-MYR@users.noreply.github.com>
2025-02-09 11:26:35 +0100

    Create magic.java

---
+++ magic.java
@@ -0,0 +1,26 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = 8;
+    //original Number
+    int magicNumber = myNumber * myNumber;
+    magicNumber += myNumber;
+    magicNumber /= myNumber;
+    magicNumber += 17;
+    magicNumber -= myNumber;
+    magicNumber /= 6;
+
+    System.out.println(magicNumber);
+
+
+
+
+    
+
+
+
+
+
+		
+
+	}
+}
